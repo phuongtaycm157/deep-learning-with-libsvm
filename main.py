12d3db01108b0c0b39707ab3226e44844348a365
Nishi <ngudai999@gmail.com>
2021-04-21 03:58:45 +0700

    export model have acc 41%

--- main.py
+++ main.py
@@ -8,23 +8,25 @@ from time import sleep
 from preprocessing_data import *
 
 # 1. get dataset
-PATH = './iris.csv'
+PATH = './images_dataset_vector.csv'
 
 data = pd.read_csv(PATH)
+# data = data.iloc[np.random.permutation(len(data))]
 
 # 2. Preprocessing
-y, y_desc = stick_label(data.species)
-X = data.iloc[:,:-1]
+# y, y_desc = stick_label(data.label)
+y = np.asarray(data.label)
+X = data.iloc[:,1:-1]
 X = np.asarray(X)
 X = precessing_many_x(X)
 
 # 3. train model
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=100)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=100)
 prob = svm_problem(y_train, X_train)
-param = svm_parameter('-t 2 -c 100')
+param = svm_parameter('-t 0 -c 100')
 result_model = svm_train(prob, param)
 
-print()
+svm_save_model('classify_images.model', result_model)
 
 # 4. test model
 
@@ -34,9 +36,9 @@ ACC, MSE, SCC = evaluations(y_test, p_label)
 print(ACC, MSE, SCC)
 
 # 4.2 Dự đoán từng phần tử .
-for x in X_test:
-    x0, max_idx = gen_svm_nodearray(x)
-    y_pre = libsvm.svm_predict(result_model, x0)
-    print(label)
-    sleep(0.5)
+# for x in X_test:
+#     x0, max_idx = gen_svm_nodearray(x)
+#     y_pre = libsvm.svm_predict(result_model, x0)
+#     print(label)
+#     sleep(0.5)
 
